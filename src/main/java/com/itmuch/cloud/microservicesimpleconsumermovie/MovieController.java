9c3baca763f16b6aa948bc536a6c5919ced27c04
dr_guanth <guantihua@163.com>
2019-01-27 16:28:09 +0800

    Initial commit

---
+++ src/main/java/com/itmuch/cloud/microservicesimpleconsumermovie/MovieController.java
@@ -0,0 +1,25 @@
+package com.itmuch.cloud.microservicesimpleconsumermovie;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author 高尚
+ * @version 1.0
+ * @date 2019/1/2716:15
+ * @desc
+ * @update update by @author on @date
+ */
+@RestController
+public class MovieController {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/user/{id}")
+    public User findById(@PathVariable Long id) {
+        return restTemplate.getForObject("http://localhost:8000/" + id, User.class);
+    }
+}
---
+++ src/main/java/com/itmuch/cloud/microservicesimpleconsumermovie/User.java
@@ -0,0 +1,58 @@
+package com.itmuch.cloud.microservicesimpleconsumermovie;
+
+import java.math.BigDecimal;
+
+/**
+ * @author 高尚
+ * @version 1.0
+ * @date 2019/1/2714:56
+ * @desc
+ * @update update by @author on @date
+ */
+public class User {
+    private Long id;
+    private String username;
+    private String name;
+    private Integer age;
+    private BigDecimal balance;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+}
